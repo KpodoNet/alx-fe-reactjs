fc5ca3da9487c9f5fcfcbc1ec2318a8afd8701f6
Joshua Kpodo <Dylanmore355@gmail.com>
2025-11-24 02:34:03 +0000

    commit components

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const API_KEY = import.meta.env.VITE_APP_GITHUB_API_KEY;
 
-export const getGitHubUser = async (username) => {
-  const response = await axios.get(`https://api.github.com/users/${username}`, {
+export const getGitHubUser = async (kpodoNet) => {
+  const response = await axios.get(`https://api.github.com/users/${kpodoNet}`, {
     headers: {
       Authorization: API_KEY ? `token ${API_KEY}` : undefined,
     },

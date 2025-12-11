c27751a81b8556d6de8c9cf00dfabbe65e6019bc
Joshua Kpodo <Dylanmore355@gmail.com>
2025-12-11 12:16:37 +0000

    modified appjs file

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,38 +1,14 @@
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
-
-
 import WelcomeMessage from './components/WelcomeMessage';
-import UserProfile from './components/UserProfile';
 
 function App() {
   return (
     <div>
       <WelcomeMessage />
-      <UserProfile 
-        name="Alice" 
-        age="25" 
-        bio="Loves hiking and photography" 
-      />
     </div>
   );
 }
 
 export default App;
 
-
-
-function App() {
-  return (
-    <div>
-      <Header />
-      <MainContent />
-      <Footer />
-    </div>
-  );
-}
-
-export default App;
 
 
